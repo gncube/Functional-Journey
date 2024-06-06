8b03aa9ffe01398c8578846214d1510e5bcd410f
Gerald Ncube <gncube@gmail.com>
2024-06-06 16:35:15 +0100

    Add project files.

---
+++ Functional Journey 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+int[] array = { 3, 9, 5, 13, 12, 30 };
+int[] query = Filter(array, GreaterThanFive);
+foreach (int value in query) { Console.WriteLine(value); }
+bool GreaterThanFive(int i) { return i > 5; }
+
+int[] Filter(int[] src, Predicate p)
+{
+    ArrayList dst = new ArrayList();
+    foreach (int value in src)
+    {
+        if (p(value))
+            dst.Add(value);
+    }
+    int[] result = new int[dst.Count];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = (int)dst[i];
+    }
+    return result;
+}
+
+delegate bool Predicate(int i);
