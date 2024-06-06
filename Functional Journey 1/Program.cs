517752e4692f1af1dd3a0e600fac6b71b32ecd25
Gerald Ncube <gncube@gmail.com>
2024-06-06 16:59:09 +0100

    Added the yield return

--- Functional Journey 1/Program.cs
+++ Functional Journey 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// C# 2
 
 int[] array = { 3, 9, 5, 13, 12, 30 };
-IEnumerable<int> query = Filter<int>(array, GreaterThanFive);
+var query = Filter<int>(array, GreaterThanFive);
 foreach (int value in query) { Console.WriteLine(value); }
 bool GreaterThanFive(int i) { return i > 5; }
 
@@ -9,8 +9,7 @@ IEnumerable<T> Filter<T>(IEnumerable<T> src, Predicate<T> p)
 {
     List<T> dst = new List<T>();
     foreach (T value in src)
-    { if (p(value)) dst.Add(value); }
-    return dst.ToArray();
+    { if (p(value)) yield return value; }
 }
 
 delegate bool Predicate<T>(T t);
