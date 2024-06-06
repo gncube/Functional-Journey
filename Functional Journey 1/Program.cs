8cbd09f73e92a0f4196a949fb83b5ec27a893c31
Gerald Ncube <gncube@gmail.com>
2024-06-06 16:47:02 +0100

    Strongly typed

--- Functional Journey 1/Program.cs
+++ Functional Journey 1/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-// C# 1
+﻿// C# 2
 
 int[] array = { 3, 9, 5, 13, 12, 30 };
 int[] query = Filter(array, GreaterThanFive);
@@ -9,13 +7,10 @@ bool GreaterThanFive(int i) { return i > 5; }
 
 int[] Filter(int[] src, Predicate p)
 {
-    ArrayList dst = new ArrayList();
+    List<int> dst = new List<int>();
     foreach (int value in src)
     { if (p(value)) dst.Add(value); }
-    int[] result = new int[dst.Count];
-    for (int i = 0; i < result.Length; i++)
-    { result[i] = (int)dst[i]; }
-    return result;
+    return dst.ToArray();
 }
 
 delegate bool Predicate(int i);
