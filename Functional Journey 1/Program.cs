604cc2e8b3ab271d6e355bf5afe6caecced81260
Gerald Ncube <gncube@gmail.com>
2024-06-06 17:33:26 +0100

    SQL in C#

--- Functional Journey 1/Program.cs
+++ Functional Journey 1/Program.cs
@@ -2,9 +2,10 @@
 
 int[] array = { 3, 9, 5, 13, 12, 30 };
 var query =
-    array
-        .Where(i => i > 5)
-        .Where(i => i % 2 == 0);
+    from i in array.AsQueryable()
+    where i > 5
+    where i % 2 == 0
+    select i;
 
 foreach (int value in query) { Console.WriteLine(value); }
 
